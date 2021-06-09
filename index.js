6ed77e69f26af5d92a9f50705f0aaae1010d01b0
ПочиковскийАнтон <79967477+PochokovskiyAnton@users.noreply.github.com>
2021-06-09 10:28:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+class Chelik {
+    constructor(name, email, ZIPcode) {
+    this.id = lastName + (i++);
+    this.email = email;
+    this.ConEmail = Conemail;
+    this.code = code;
+    this.interface = `
+    chelik ${this.counter}:
+    Email is ${this.email},
+    ConEmail is ${this.Conemail}
+    Code is ${this.code}
+    <p></p>
+    `;
+    }
+  }
